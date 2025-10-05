2fb282133041407de7b05c2f542b573226f70803
Pooja1737 <poojayadav17112006@gmail.com>
2025-10-05 12:33:35 +0530

    Create plates.py

---
+++ plates.py
@@ -0,0 +1,26 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+def is_valid(s):
+    if not (2 <= len(s) <= 6):
+        return False
+    if not (s[0].isalpha() and s[1].isalpha()):
+        return False
+    number_started = False
+    for i, char in enumerate(s):
+        if not char.isalnum():
+            return False
+        if char.isdigit():
+            if not number_started:
+                if char == "0":
+                    return False
+                number_started = True
+        else:
+            if number_started:
+                return False
+    return True
+if __name__ == "__main__":
+    main()
