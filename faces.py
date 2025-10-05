69eb3d71ae95fa99e502ba2b6347ec88c8465d79
Pooja1737 <poojayadav17112006@gmail.com>
2025-10-05 11:04:36 +0530

    Create faces.py

---
+++ faces.py
@@ -0,0 +1,8 @@
+def convert(text):
+    text = text.replace(":)", "🙂")
+    text = text.replace(":(", "🙁")
+    return text
+def main():
+    user_input = input("Enter some text: ")
+    print(convert(user_input))
+main()
